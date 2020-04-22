7226551283e30ab91345d81593f9909b344d3036
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2020-04-23 09:12:00 +1000

    completed 2.12

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/Search'
 import PersonForm from './components/PersonForm'
 import Numbers from './components/Numbers'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [ persons, setPersons ] = useState([])
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ showPeople, setShowPeople ] = useState('') 
+
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+    console.log('render', persons.length, 'people')
 
     const addPerson = (event) => {
         event.preventDefault()
